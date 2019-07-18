d2f40b0e2ff9411291f8ec317a69b559af04201a
Desaree Byers <dariusbrown@Dariuss-MacBook-Pro.local>
2019-07-18 14:10:57 -0500

    initial commit

---
+++ src/main/java/com/company/U1M3SummativeByersDesaree/U1M3SummativeByersDesareeApplication.java
@@ -0,0 +1,13 @@
+package com.company.U1M3SummativeByersDesaree;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class U1M3SummativeByersDesareeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(U1M3SummativeByersDesareeApplication.class, args);
+	}
+
+}
